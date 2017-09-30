16cd0c0ccd8fac40eedd1e30d1b4f4579fd94ddb
Christian Nunciato <cnunciato@chef.io>
2017-09-30 10:11:56 -0700

    Update the depot URL

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -3,5 +3,5 @@
 source /scripts/env.sh
 
 for pkg in /hab/cache/artifacts/core*.hart; do
-  hab pkg upload --url http://localhost:9636/v1/depot --auth "$HAB_AUTH_TOKEN" "$pkg" --channel stable
+  hab pkg upload --url http://localhost:9636/v1 --auth "$HAB_AUTH_TOKEN" "$pkg" --channel stable
 done
