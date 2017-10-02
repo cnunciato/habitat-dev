453e5fbc0ca113ccab6d65b60eac8bf89e789aa8
Christian Nunciato <cnunciato@chef.io>
2017-10-02 09:30:46 -0700

    Update the psql script

--- scripts/psql.sh
+++ scripts/psql.sh
@@ -2,6 +2,7 @@
 
 for shard in {0..127}
 do
-  sql="SET SEARCH_PATH TO shard_$shard; DROP FUNCTION IF EXISTS insert_origin_package_v2(bigint, bigint, text, text, text, text, text, text, text, text, text);"
+  echo $shard
+  sql="SET SEARCH_PATH TO shard_$shard; SELECT name FROM origins;"
   echo "$sql" | "$(hab pkg path core/postgresql)/bin/psql" -U hab builder_originsrv
 done
